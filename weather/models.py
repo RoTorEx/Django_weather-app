33155ac0a9c6d9838ec044fe3f39f9cb32bcf7af
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-04-07 11:59:35 +0300

    add: City model, loop in temaplate
    The model with cities is filled in, a loop is added to enumerate cities.

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    '''Класс создаёт табличку City с полем name и возращает название города'''
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from .models import City
+import requests
 
 
 def index(request):
-    return render(request, 'weather/index.html')  # Функция вызова шаблона главной страницы
+    app_id = "acc48f91d35aafd02e3b9f305c82243f"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + app_id
+
+    cities = City.objects.all()  # Делаем выборку из таблицы City
+
+    all_cities = []
+
+    for city in cities:
+        response = requests.get(url.format(city)).json()  # Получаем ответ в json формате и конвертируем его в словарь
+
+        city_info = {
+            "city": city.name,
+            "temp": response["main"]["temp"],
+            "icon": response["weather"][0]["icon"],
+        }
+
+        all_cities.append(city_info)
+
+    context = {"all_info": all_cities}
+
+    return render(request, 'weather/index.html', context)  # Функция вызова шаблона главной страницы
